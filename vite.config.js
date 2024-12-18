42d2ee853c0901472b8dceb3f2c0f62a27924fb8
huang <clairehuang77777@gmail.com>
2024-12-18 13:47:20 +0800

    add structured-clone

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
-
+// 引入 structuredClone 的 Polyfill
+import 'structured-clone'
 
 // <https://vitejs.dev/config/>
 export default defineConfig({
