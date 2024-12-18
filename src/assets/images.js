c4a52b4d18d491905ddeca2063427fedf169aaad
huang <clairehuang77777@gmail.com>
2024-12-18 15:29:16 +0800

    fix img error try 1

---
+++ src/assets/images.js
@@ -0,0 +1,27 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
+export const images = {
+  back:`${BASE_URL}back.png`,
+  bell:`${BASE_URL}bell.png`,
+  bell_click:`${BASE_URL}bell_click.png`,
+  close:`${BASE_URL}close.png`,
+  comment:`${BASE_URL}comment.png`,
+  header:`${BASE_URL}header.png`,
+  header2:`${BASE_URL}header2.png`,
+  heart:`${BASE_URL}heart.png`,
+  home:`${BASE_URL}home.png`,
+  home_click:`${BASE_URL}home_click.png`,
+  house:`${BASE_URL}house.png`,
+  logout-click:`${BASE_URL}logout-click.png`,
+  logout:`${BASE_URL}logout.png`,
+  mail:`${BASE_URL}mail.png`,
+  mail_click:`${BASE_URL}mail_click.png`,
+  profile:`${BASE_URL}profile.png`,
+  profile_click:`${BASE_URL}profile_click.png`,
+  red-heart:`${BASE_URL}red-heart.png`,
+  setting:`${BASE_URL}setting.png`,
+  setting_click:`${BASE_URL}setting_click.png`,
+  user:`${BASE_URL}user.png`,
+  user2:`${BASE_URL}user2.png`
+};
+
--- src/components/display/left/LeftSection.jsx
+++ src/components/display/left/LeftSection.jsx
@@ -1,6 +1,8 @@
 import { AClogo } from '../../../assets/image/AC-logo'
 import { LeftSectionButton } from './LeftSectionButton'
 import './LeftSection.scss'
+import { images } from "../../../assets/images"
+
 
 
 export const LeftSection = () => {
@@ -11,14 +13,14 @@ export const LeftSection = () => {
           <div className="left-section-flex-start-logo">
           <AClogo />
           </div>
-          <LeftSectionButton src="/home.png" srcClick="/home_click.png" title="首頁" id={4}/>
-          <LeftSectionButton src="/profile.png" srcClick="/profile_click.png" title="個人資料" id={5}/>
-          <LeftSectionButton src="/setting.png" srcClick="/setting_click.png" title="設定" id={6}/>
+          <LeftSectionButton src={images.home}srcClick="/home_click.png" title="首頁" id={4}/>
+          <LeftSectionButton src={images.profile} srcClick="/profile_click.png" title="個人資料" id={5}/>
+          <LeftSectionButton src={images.setting} srcClick="/setting_click.png" title="設定" id={6}/>
           <button type="button" className='LeftSection-Tweet-Btn'>推文</button>
         </div>
         <div className="left-section-flex-center"></div>
         <div className="left-section-flex-end">
-          <LeftSectionButton src="/logout.png" srcClick="/logout-click.png" title="登出" id={7}/>
+          <LeftSectionButton src={images.logout} srcClick="/logout-click.png" title="登出" id={7}/>
         </div>
       </div>
     </>
