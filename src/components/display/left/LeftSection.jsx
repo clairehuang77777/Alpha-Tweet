72d361d1b838948052bd8820581fe8bde276a65b
huang <clairehuang77777@gmail.com>
2024-12-18 16:37:09 +0800

    fix click png error

--- src/components/display/left/LeftSection.jsx
+++ src/components/display/left/LeftSection.jsx
@@ -13,14 +13,14 @@ export const LeftSection = () => {
           <div className="left-section-flex-start-logo">
           <AClogo />
           </div>
-          <LeftSectionButton src={images.home}srcClick="/home_click.png" title="首頁" id={4}/>
-          <LeftSectionButton src={images.profile} srcClick="/profile_click.png" title="個人資料" id={5}/>
-          <LeftSectionButton src={images.setting} srcClick="/setting_click.png" title="設定" id={6}/>
+          <LeftSectionButton src={images.home} srcClick={images.home_click} title="首頁" id={4}/>
+          <LeftSectionButton src={images.profile} srcClick={images.profile_click} title="個人資料" id={5}/>
+          <LeftSectionButton src={images.setting} srcClick={images.setting_click} title="設定" id={6}/>
           <button type="button" className='LeftSection-Tweet-Btn'>推文</button>
         </div>
         <div className="left-section-flex-center"></div>
         <div className="left-section-flex-end">
-          <LeftSectionButton src={images.logout} srcClick="/logout-click.png" title="登出" id={7}/>
+          <LeftSectionButton src={images.logout} srcClick={images.logout_click} title="登出" id={7}/>
         </div>
       </div>
     </>
