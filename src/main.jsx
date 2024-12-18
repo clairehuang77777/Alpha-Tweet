eada83f8a41fd3070fdc3dd003dd3758cf00d9fb
huang <clairehuang77777@gmail.com>
2024-12-18 13:52:44 +0800

    fix structured-clone error and update node-v

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import structuredClone from 'structured-clone';
+
+if (typeof globalThis.structuredClone === 'undefined') {
+  globalThis.structuredClone = structuredClone;
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
